c7b3f5750aae11ffd178b78b16e08be9cda431f4
Danny <ducanh.danny@gmail.com>
2020-10-28 11:50:04 +0700

    Update default_popup.js

--- default_popup.js
+++ default_popup.js
@@ -1,11 +1,10 @@
-let date = new Date();
-// let time = date.toLocaleTimeString();
-let hour = date.getHours();
+const date = new Date();
+// const time = date.toLocaleTimeString();
+const hour = date.getHours();
 
 document.querySelector(".content").addEventListener("click", () => {
   if (hour < 6 || hour > 22) {
-    //   if (1) {
-    let code = `<img src="/bg/block.gif" alt="&#10084;" />
+    const code = `<img src="/bg/block.gif" alt="&#10084;" />
         <p>Sorry, Facebook is blocking!</p>`;
     document.getElementById("realTime").innerHTML = null;
     document.getElementById("realTime").insertAdjacentHTML("beforeend", code);
